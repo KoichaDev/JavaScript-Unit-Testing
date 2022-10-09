ebdb3836fa9836f82f17435f3fbe66d8e6b78ea2
Khoi Hoang <khoi.hoang@bouvet.no>
2022-10-09 22:42:02 +0200

    init: 🎉 More on mocking and diving into front-end

---
+++ mocking-spies-dealing-with-side-effects-node-js/src/util/logger.js
@@ -0,0 +1,6 @@
+// Logging to the console that interqacts with the system console (which is an external system)
+// and therefore, this is technically also a side-effect
+
+export default function log(message) {
+	console.log(message);
+}

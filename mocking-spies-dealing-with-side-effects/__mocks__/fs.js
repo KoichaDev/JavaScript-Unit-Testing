715bab28d8851a1fa5e8a8ee3e87eda37ffa5778
Khoi Hoang <khoi.hoang@bouvet.no>
2022-10-09 22:42:59 +0200

    asd

--- mocking-spies-dealing-with-side-effects/__mocks__/fs.js
+++
@@ -1,9 +0,0 @@
-import { vi } from 'vitest';
-
-export const promises = {
-	writeFile: vi.fn((path, data) => {
-		return new Promise((resolve, reject) => {
-			resolve();
-		});
-	}),
-};
--- mocking-spies-dealing-with-side-effects/src/util/logger.js
+++
@@ -1,6 +0,0 @@
-// Logging to the console that interqacts with the system console (which is an external system)
-// and therefore, this is technically also a side-effect
-
-export default function log(message) {
-	console.log(message);
-}
